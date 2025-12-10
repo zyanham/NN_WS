9648d23ca44b70190e062de1ef4a8b75f1ad30e2
zyanham <zyangalianhamster01@gmail.com>
2025-12-10 18:52:35 +0900

    update

--- YOLOX_WS/04_gen_snapshot.bash
+++ YOLOX_WS/04_gen_snapshot.bash
@@ -1,50 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# YOLOX_WS のルート
-NN_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${NN_ROOT}"
+yolox_cfg_list=(
+  "x",
+  "l",
+  "m",
+  "s",
+  "tiny",
+  "nano"
+)
 
-# 事前に 02_run_host_image.bash で使っている setup.bash を再利用
-# これで venv 有効化 & YOLOX インストールなどが揃う前提
+npu_ip_list=(
+  "VE2802_NPU_IP_O00_A304_M3",
+  "ZU7EV_NPU_IP_1102",
+  "ZU7EV_NPU_IP_1108"
+)
 
-# 引数
-MODEL="${1:-nano}"        # nano / tiny / s / m / l / x
-IMGSZ="${2:-416}"         # 416 / 640 など
-IMG_ARG="${3:-../../NN_WS_v5.1/Dataset/coco2017/Dataset/COCO30}"  # キャリブ用画像ディレクトリ
-
-# SNAPSHOT 出力先ルート
-SNAP_ROOT="${NN_ROOT}/SNAPSHOTS"
-mkdir -p "${SNAP_ROOT}"
-
-# ★スナップショット名（NPU IP 名はとりあえず手動運用にしておく）
-SNAP_NAME="SNAP.V5.1_YOLOX_${MODEL}_${IMGSZ}"
-
-export VAISW_SNAPSHOT_DIRECTORY="${SNAP_ROOT}/${SNAP_NAME}"
-# サマリを出したければコメント解除
-# export VAISW_RUNSESSION_SUMMARY=1
-
-echo "[INFO] YOLOX_WS snapshot generation"
-echo "[INFO] ROOT                    : ${NN_ROOT}"
-echo "[INFO] MODEL                   : ${MODEL}"
-echo "[INFO] INPUT SIZE              : ${IMGSZ}"
-echo "[INFO] IMAGE / DIR FOR CALIB   : ${IMG_ARG}"
-echo "[INFO] VAISW_SNAPSHOT_DIRECTORY: ${VAISW_SNAPSHOT_DIRECTORY}"
-echo
-
-# 注意:
-#  - ここで VAISW の NPU スタックはすでに有効になっている想定
-#    → 事前に 'source npu_ip/settings.sh <IP名>' を実行しておくこと
-#  - src/run_host_image.py は PyTorch (PTH) 実装版を使う
-
-python src/run_host_image.py \
-  --model "${MODEL}" \
-  --imgsz "${IMGSZ}" \
-  --image "${IMG_ARG}" \
-  --score 0.1
-
-echo
-echo "[INFO] Snapshot run finished."
-echo "[INFO] Snapshots should be under:"
-echo "       ${VAISW_SNAPSHOT_DIRECTORY}"
+for npu_ip in "${npu_ip_list[@]}"; do
+source ${VITIS_AI_REPO}/npu_ip/settings.sh ${npu_ip}
+  for yolox_cfg in "${yolox_cfg_list[@]}"; do
+    VAISW_SNAPSHOT_DUMPIOS=5 \
+    VAISW_SNAPSHOT_DIRECTORY=SNAPSHOTS/SNAP.${npu_ip}_YOLOX${yolox_cfg}_b1 \
+    VAISW_RUNOPTIMIZATION_DDRSHAPE=N_C_H_W_c \
+    VAISW_QUANTIZATION_NBIMAGES=1 \
+    ./run assets/dog.jpg ${yolox_clg}
+  done
+done
 
---
+++ YOLOX_WS/setup2.bash
@@ -0,0 +1,6 @@
+cp -r /home/demo/YOLOX ./original/YOLOX_DEMO
+ln -s original/YOLOX_DEMO/demo .
+ln -s original/YOLOX_DEMO/tools .
+ln -s original/YOLOX_DEMO/yolox .
+ln -s original/YOLOX_DEMO/mipso_custom.py .
+ln -s original/YOLOX_DEMO/assets .
